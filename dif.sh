568a922e122e3bddaf71cc4eae94d06c818580b6
ken <block24block@gmail.com>
2014-06-28 11:57:57 +0800

    dif tuning

--- dif.sh
+++ dif.sh
@@ -7,9 +7,9 @@ other=$(echo $bmlog | cut -d' ' -f2- )
 
 for nex in $other
 do
-git diff $pre $nex > difdetail.$$
-rsub=$(grep '^[-]' difdetail.$$ | grep 'HREF' | wc -l )
-radd=$(grep '^[+]' difdetail.$$ | grep 'HREF' | wc -l )
+git diff $pre $nex | grep 'HREF' > difdetail.$$
+rsub=$(grep '^[-]' difdetail.$$ | wc -l )
+radd=$(grep '^[+]' difdetail.$$ | wc -l )
 echo $(date --date=$(git log $nex --oneline | head -n1 | awk '{print $2}' ) +%y%m%d ) $rsub $radd
 pre=$nex
 done
