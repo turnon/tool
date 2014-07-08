33c8bcb8d8ece456c8d734f8d5ef4609f7e52c9f
ken <block24block@gmail.com>
2014-07-08 11:14:29 +0800

    remove space

--- hist.sh
+++ hist.sh
@@ -4,6 +4,6 @@ history -w
 
 cat ~/.bash_history >> hist.txt
 
-sed -e 's/\r//g' hist.txt | sort -u > hist.$$
+sed -e 's/\r//g' hist.txt | sed -e 's/[ ]$//' | sort -u > hist.$$
 
 mv hist.$$ hist.txt
