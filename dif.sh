073a769a5e205afeced69e081c6b6e7f9c4a365c
k <k@wh.com>
2014-07-13 22:31:30 +0800

    err at bottom

--- dif.sh
+++ dif.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+main(){
 bmlog=$(git log --oneline --reverse | grep '[0-9]$'  | awk '{print $1}')
 
 pre=$(echo $bmlog | cut -d' ' -f1)
@@ -7,11 +8,16 @@ other=$(echo $bmlog | cut -d' ' -f2- )
 
 for nex in $other
 do
-git diff $pre $nex | grep 'HREF' > difdetail.$$
+git diff $pre $nex | grep 'HREF' >difdetail.$$
 rsub=$(grep -c '^[-]' difdetail.$$ )
 radd=$(grep -c '^[+]' difdetail.$$ )
 echo $(date --date=$(git log $nex --oneline | head -n1 | awk '{print $2}' ) +%y%m%d ) $rsub $radd
 pre=$nex
 done
+}
 
-rm -f difdetail.$$
+main 2>$$.err
+
+cat $$.err
+
+rm -f difdetail.$$ $$.err
