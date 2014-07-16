a23d9739783564e600877c2e947aa1311c60a7aa
ken <block24block@gmail.com>
2014-07-16 13:50:03 +0800

    finish ctname

--- ctname.sh
+++ ctname.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+cd $1
+
 file_create_time(){
-while read time1 time2 time3 orgname
+while read time1 time2 time3 time4 time5 orgname
 do
-echo $(date --date="$time1 $time2 $time3" +%y%m%d%H%M%S ) $orgname
+echo $(date --date="$time1 $time2 $time3 $time4 $time5" +%y%m%d%H%M%S ) $orgname
 done<<EOF
-$(ls -cl $1 | awk '{print $6" "$7" "$8" "$9}')
+$(ls -cl --full-time $1 | grep '^-' | awk '{$1=" ";$2=" ";$3=" ";$4=" ";$5=" ";print $0}')
 EOF
 }
 
 while read newtime orgname
 do
-echo $orgname $newtime
+mv -v "$orgname" "$newtime"_"$orgname"
 done<<EOF
-$(file_create_time $1)
+$(file_create_time . )
 EOF
