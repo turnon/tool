3abef9601dd9a0625435684cedfb72a49b7fc778
ken <block24block@gmail.com>
2014-09-15 21:07:55 +0800

    use pipe

--- dusort.sh
+++ dusort.sh
@@ -17,14 +17,12 @@ do
 NEW_SIZE=$(encode "$(echo "$SIZE" | sed 's/[A-Z]/ &/' | sed 's/[0-9]$/& B/' )")
 NEW_NAME=$(basename "$NAME")$([ -d "$NAME" ] && echo '/' )
 echo $NEW_SIZE $NEW_NAME
-done<<EOF
-$(du "$1"/* -h --max-depth=0)
-EOF
+done
 }
 
 getsort()
 {
-humansize "$1" | sort -k2n,2 -k1n,1
+sort -k2n,2 -k1n,1
 }
 
 tricol()
@@ -33,10 +31,8 @@ while read SIZE1 SZIE2 NAME
 do
 NEW_SZIE2=$(decode "$SZIE2")
 printf '%6.1f %1s  %-20s\n' $SIZE1 $NEW_SZIE2 "$NAME"
-done<<EOF
-$(getsort "$1" )
-EOF
+done
 }
 
 ## main
-tricol $([ -z "$1" ] && echo '.' || echo "$1")
+du "$([ -z "$1" ] && echo '.' || echo "$1")"/* -h --max-depth=0 | humansize | getsort | tricol
