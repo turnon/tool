be24f37acf2a47235080854a359a37d11f230038
k <k@wh.com>
2014-09-16 17:14:37 +0800

    custom times

--- times.sh
+++ times.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-read cmd
+read cmd1 cmd2
+
+expr "$cmd1" + 1 >/dev/null 2>&1 \
+&& { times=$cmd1 ; cmd=$cmd2 ; } \
+|| { times=10 ; cmd="$cmd1 $cmd2" ; }
 
 run()
 {
-for i in {1..10}
+while [ "$times" -ne 0 ]
 do
+times=$( expr "$times" - 1 )
 time $cmd
 done
 }
