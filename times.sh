02b3cdc583b11383813f18bdaeb1aa7803eb15b2
ken <block24block@gmail.com>
2014-09-16 22:07:25 +0800

    average

--- times.sh
+++ times.sh
@@ -18,9 +18,13 @@ done
 { run ; } 2>>tmp.$$
 
 grep '^real' tmp.$$ \
-| sed 's/real//' | sed 's/[ms]/ /g' \
+| cut -f2  | sed 's/[ms]/ /g' \
 | cat -n \
 | sort -k2n,2 -k3n,3 \
-| cat -n
+| cat -n \
+| awk 'BEGIN {print;printf "%3s  %3s\n","sor","seq"} \
+ {printf "%3s  %3s  %2d m %6.3f s\n",$1,$2,$3,$4} \
+ {sum_mi=sum_mi+$3;sum_se=sum_se+$4} \
+  END {sum_mi=sum_mi/NR;sum_se=sum_se/NR;printf "\naverage : %2d m %6.3f s\n",sum_mi,sum_se}'
 
 rm tmp.$$
