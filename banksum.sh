c7ff273b94085a269ad5af9aae5b8032dd3db1f6
ken <block24block@gmail.com>
2014-10-05 14:25:46 +0800

    directly semicolon

--- banksum.sh
+++ banksum.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# replace semicolon
-rcl()
-{
-[ "$1" = ';' ] && echo 0 || echo ${1%\;}
-}
-
 # date format yyyymmdd
 dat()
 {
@@ -17,17 +11,12 @@ fmt()
 {
 while read a b c d e f g
 do
-a=$(rcl "$a")
-b=$(dat $(rcl "$b"))
-c=$(rcl "$c")
-d=$(rcl "$d")
-e=$(rcl "$e")
-f=$(rcl "$f")
-g=$(rcl "$g")
+b=$(dat "$b")
 printf '%16s %6s %8s %9.2f %9.2f %9.2f %-50s\n' "$a" "$b" "$c" "$d" "$e" "$f" "$g"
 done
 }
 
 grep -h '\;$' "$1"* \
+| awk -F';' '{printf "%s %s %s %f %f %f %s\n",$1,$2,$3,$4,$5,$6,$7}' \
 | fmt \
-| sort -u 
+| sort -u
