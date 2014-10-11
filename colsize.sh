3dde8416bac41f8643ccec2c66d5b86f8463b801
ken <block24block@gmail.com>
2014-10-11 23:57:13 +0800

    colsize not using while

--- colsize.sh
+++ colsize.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-while read line
-do
-echo "$line" | awk '{for(i=1;i<=NF;i++)print length($i)}' | xargs
-done
+awk '{for(i=1;i<=NF;i++) if(i==NF){printf "%.0f\n",length($i)}else{printf "%.0f ",length($i)}}' \
+| awk '{if(NF>mnf){mnf=NF};for(i=1;i<=NF;i++) if($i>a[i]){a[i]=$i}} \
+END{for(k=1;k<=mnf;k++) if(k==mnf){printf "%.0f\n",a[k]}else{printf "%.0f ",a[k]}}'
 
 exit
 
