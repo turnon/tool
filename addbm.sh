36cd7db3136e45e7fd5fc9b39ac932427a11b3c2
ken <block24block@gmail.com>
2014-10-11 17:53:21 +0800

    new chrome bookmark format

--- addbm.sh
+++ addbm.sh
@@ -6,7 +6,7 @@ cd $1
 
 for f in $(ls bookm* 2>/dev/null)
 do
-newname=$(echo $f | cut -d_ -f2)
+newname=$(echo $f | sed 's/bookmarks_//' | tr '_'  '-')
 orgdate=$(echo $f | cut -d_ -f2 | cut -d. -f1)
 cleanicon $f >$newname
 touch $newname -m -d $orgdate
