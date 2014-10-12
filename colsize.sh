ba4b2d5871b0c29a8f5d4fb9af6612f17a4228bc
k <k@wh.com>
2014-10-12 22:14:30 +0800

    colsize storeflow coltorow

--- colsize.sh
+++ colsize.sh
@@ -11,22 +11,28 @@ flow \
 END{for(k=1;k<=mnf;k++) if(k==mnf){printf "%.0f\n",a[k]}else{printf "%.0f ",a[k]}}' \
 )
 
-echo $lens
-
-exit
-
 cols=$(echo $lens | wc -w)
 
-para=$(seq.sh "$cols" | xargs)
+para=$(seq.sh "$cols" | xargs | sed 's/[0-9]*/\$&\,/g' | sed 's/\,$//')
 
 types=$(
-for m in $para
+m=1
+while [ $m -le "$cols" ]
 do
 { flow \
 | awk -v n=$m '{print $n}' \
-| grep -E '[a-zA-Z:]' >/dev/null ; } && echo 's' || echo '.0f'
+| grep -E '[a-zA-Z:]' ; } >/dev/null && echo 's' || echo 'd'
+m=$(expr "$m" + 1 )
 done | xargs \
 )
 
-echo $lens
-echo $types
+formatp=$( \
+{ echo $lens;echo $types; } \
+| coltorow.sh \
+| awk '{if($2=="s"){printf "%-6s","%-"$1$2}else{printf "%-6s","%"$1$2}} END{printf "%1s","\n"}' \
+)
+
+formatp='"'$formatp'\n",'"$para"
+
+flow \
+| awk '{printf '"$formatp"'}'
