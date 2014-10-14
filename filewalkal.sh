b1eec0790d5adeb1ab286f29b31f580f8493f328
ken <block24block@gmail.com>
2014-10-14 20:24:22 +0800

    hidden file

--- filewalkal.sh
+++ filewalkal.sh
@@ -4,8 +4,8 @@ toscan=$( { [ -z "$1" ] && echo "." || echo "$1" ; } | sed 's/\/$//')
 
 [ -z "$2" ] && ls -ld "$toscan"
 
-for f in "$toscan"/*
+for f in "$toscan"/.* "$toscan"/*
 do
-[ -e "$f" ] || continue
+[ -e "$f" ] && [ ! "$f" -ef "$toscan" ] && [ ! "$f" -ef "$toscan"'/..' ] || continue
 [ -d "$f" ] && { ls -ld "$f"  ; $0 "$f" 'not root'; } || ls -l "$f"
 done
