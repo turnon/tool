f3ed6cabd8ded027b9089257bf6a663a3dd3f359
k <k@wh.com>
2014-10-14 20:33:00 +0800

    lastcol.sh 2.0

--- lastcol.sh
+++ lastcol.sh
@@ -2,34 +2,31 @@
 
 . storeflow.fn
 
-addslash()
-{
-echo $1 | sed 's/./\\&/g'
-}
-
-rmfirstcol()
-{
-while read line
-do
-firstcol=$(echo "$line" | awk '{print $1}')
-echo "$line" | sed 's/^'"$firstcol"'//'
-done
-}
-
 col="$1"
 
+rmfstcol()
+{
+awk '{sub($1,"");print $0}' "$1"
+}
+
+alignleft()
+{
+while read noheadsp
+do
+echo $noheadsp
+done <"$1"
+}
+
+# main
 store lstcl.$$ >/dev/null
 
 while [ "$col" -gt 1 ]
 do
-cat lstcl.$$ | rmfirstcol >lstcltmp.$$
+rmfstcol lstcl.$$ >lstcltmp.$$
 mv lstcltmp.$$ lstcl.$$
 col=$(expr "$col" - 1)
 done
 
-while read noheadsp
-do
-echo $noheadsp
-done <lstcl.$$
+alignleft lstcl.$$
 
 rm lstcl.$$
