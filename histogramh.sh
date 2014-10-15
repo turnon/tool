cb0dc462a9e11440a74f726d0998bd9c58d4d357
ken <block24block@gmail.com>
2014-10-15 17:19:43 +0800

    reduce graph length

--- histogramh.sh
+++ histogramh.sh
@@ -2,6 +2,8 @@
 
 # x=$2, y=$1
 
-awk '{x[NR]=$2;y[NR]=$1; \
+[ -z "$1" ] && reduce=1 || reduce="$1"
+
+awk -v rd=$reduce '{x[NR]=$2;y[NR]=int($1/rd); \
 xlentmp=length($2);if(xlen<xlentmp)xlen=xlentmp} \
 END{for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(k=1;k<=y[i];k++)printf "%s","/";printf "%s\n",y[i]}}'
