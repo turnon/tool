be26defa6276cb1b04644acdd390060f0a39373d
k <k@wh.com>
2014-10-15 19:26:35 +0800

    bug "reduce"

--- histogramh.sh
+++ histogramh.sh
@@ -4,6 +4,6 @@
 
 [ -z "$1" ] && reduce=1 || reduce="$1"
 
-awk -v rd=$reduce '{x[NR]=$2;y[NR]=int($1/rd); \
+awk -v rd=$reduce '{x[NR]=$2;y[NR]=$1; \
 xlentmp=length($2);if(xlen<xlentmp)xlen=xlentmp} \
-END{for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(k=1;k<=y[i];k++)printf "%s","/";printf "%s\n",y[i]}}'
+END{for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(k=1;k<=int(y[i]/rd);k++)printf "%s","/";printf "%s\n",y[i]}}'
