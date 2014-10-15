4a2299499ee4c803f0f831808234ed617d4416cc
ken <block24block@gmail.com>
2014-10-15 16:53:07 +0800

    histogram style

--- histogramh.sh
+++ histogramh.sh
@@ -4,4 +4,4 @@
 
 awk '{x[NR]=$2;y[NR]=$1; \
 xlentmp=length($2);if(xlen<xlentmp)xlen=xlentmp} \
-END{for(i=1;i<=NR;i++){printf "%"xlen"s",x[i];for(k=1;k<=y[i];k++)printf "%s","-";printf "%s\n",y[i]}}'
+END{for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(k=1;k<=y[i];k++)printf "%s","/";printf "%s\n",y[i]}}'
