d38d5f7f76659d55fa75f3b83d159172da6fe488
k <k@wh.com>
2014-10-16 21:26:59 +0800

    comment

--- uniqrc.sh
+++ uniqrc.sh
@@ -2,19 +2,23 @@
 
 # uniq --range -c
 
+# function to create range
 range()
 {
 step.sh "$@" | sed '$d' | xargs
 step.sh "$@" | sed -n '2,$p' | xargs
 }
 
+# create range and store it in tmp file
 range "$@" | coltorow.sh | cat -n > uniqrc.$$
 
+# initial array
 while read row min max
 do
 arr[$row]=0
 done < uniqrc.$$
 
+# count
 while read record
 do
 while read row min max
@@ -23,9 +27,11 @@ do
 done < uniqrc.$$
 done
 
+# print result
 while read row min max
 do
 echo ${arr[$row]} $min '-' $max
 done < uniqrc.$$
 
+# del tmp
 rm uniqrc.$$
