39d179e66eecdddcab413c5d29f28fb753b0fe45
k <k@wh.com>
2014-10-17 17:53:31 +0800

    get right date format

--- addbm.sh
+++ addbm.sh
@@ -7,7 +7,7 @@ cd $1
 for f in $(ls bookm* 2>/dev/null)
 do
 newname=$(echo $f | sed 's/bookmarks_//' | tr '_'  '-')
-orgdate=$(echo $f | cut -d_ -f2 | cut -d. -f1)
+orgdate=$(echo $newname | cut -d. -f1)
 cleanicon $f >$newname
 touch $newname -m -d $orgdate
 rm -v $f
