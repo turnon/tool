90891b123f843488e1ccdc3c03dab5490b30cbef
k <k@wh.com>
2014-10-18 15:47:46 +0800

    percent shell script

---
+++ percgram.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#!/bin/sh
+
+# x=$2, y=$1
+
+[ -z "$1" ] && reduce=1 || reduce="$1"
+
+alignleft.sh \
+| awk -v rd=$reduce '{y[NR]=$1;sub($1,"");x[NR]=$0; \
+xlentmp=length($0);if(xlen<xlentmp)xlen=xlentmp} \
+END{j=0;for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(m=1;m<=j;m++){printf "%s"," "};for(k=1;k<=int(y[i]/rd);k++)printf "%s","/";printf "%s\n",y[i];j=j+k-1}}'
+
