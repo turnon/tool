fc17f09fdf83a12bed99441bc90d9a1c725b9712
ken <block24block@gmail.com>
2014-10-18 16:02:08 +0800

    percent style

--- percgram.sh
+++ percgram.sh
@@ -9,5 +9,5 @@
 alignleft.sh \
 | awk -v rd=$reduce '{y[NR]=$1;sub($1,"");x[NR]=$0; \
 xlentmp=length($0);if(xlen<xlentmp)xlen=xlentmp} \
-END{j=0;for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(m=1;m<=j;m++){printf "%s"," "};for(k=1;k<=int(y[i]/rd);k++)printf "%s","/";printf "%s\n",y[i];j=j+k-1}}'
+END{j=0;for(i=1;i<=NR;i++){printf "%"xlen"s : ",x[i];for(m=1;m<=j;m++){printf "%s"," "};for(k=1;k<=int(y[i]/rd);k++)printf "%s","/";printf "%s%s\n",y[i],"%";j=j+k-1}}'
 
