e33cfb33a5a5b3eafa77ca8366100703a2d61cb3
k <k@wh.com>
2015-11-02 23:59:14 +0800

    --full-time & bookmark name format change

--- addbm.sh
+++ addbm.sh
@@ -5,11 +5,12 @@
 cd $1
 
 ls --full-time bookm* \
-| awk '{print $7,$8,$9,$10,$11}' \
-| while read d1 d2 d3 d4 name
+| awk '{print $6,$7,$9}' \
+| while read d1 t2 name
 do
-newname="$(echo $name | sed 's/[^0-9]/ /g' | awk '{printf "%2.0f%2.0f%2.0f\n",$1,$2,$3}')"'.html'
+datestr=$(echo $name | sed 's/bookmarks_//g' | sed 's/\.html//g' | tr '_' '-')
+newname=$(date -d $datestr +%y%m%d)'.html'
 cleanicon $name >$newname
-touch $newname -m -d "$d1 $d2 $d3 $d4"
+touch $newname -m -d "$d1 $t2"
 rm -v $name
 done
