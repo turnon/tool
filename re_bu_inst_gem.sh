efc275c29ed7a91fdf7df9233ae5ad3014ffc54a
k <k@wh.com>
2016-02-21 19:27:30 +0800

    re build and install gem

---
+++ re_bu_inst_gem.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+version=$1
+spec_name=$(ls *.gemspec)
+gem_name=${spec_name%%.*}
+gem_pack="${gem_name}-${version}.gem"
+
+rm ${gem_pack}
+gem uninstall ${gem_name}
+gem build ${spec_name}
+gem install ./${gem_pack}
