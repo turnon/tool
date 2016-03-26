a843503812d2056aaa0f7f459b3aa91097c29e8a
ken <block24block@gmail.com>
2016-03-26 17:14:34 +0800

    es.sh --pwd to search in current dir tree

--- es.sh
+++ es.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-es $@ | sed 's/\\/\//g' | sed 's/^\([A-Z]\):/\/\1/'
+# windows path to unix path
+w2u(){
+  sed 's/\\/\//g' | sed 's/^\([A-Z]\):/\/\1/'
+}
+
+# unix path to windows path
+u2w(){
+  sed 's/^\///' | sed 's/^\([a-z]\)/\1:/' | sed 's/\//\\/g'
+}
+
+# query in current dir tree ?
+if [ $1 == "--pwd" ]
+then
+  path=$(pwd | u2w)
+  shift
+  q="$path $*"
+else
+  q="$*"
+fi
+
+# execute query
+es $q | w2u
