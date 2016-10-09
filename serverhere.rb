02b54a368a5692dffd8f05032bc020fe0fc56c40
ken <block24block@gmail.com>
2016-10-09 21:24:42 +0800

    last-modified and content-type css

--- serverhere.rb
+++ serverhere.rb
@@ -6,12 +6,15 @@ require 'socket'
 rack_proc = lambda do |env|
 	path = File.join('.', env['REQUEST_PATH'])
 	if File.exists? path
+	  last_mod = File.mtime(path).strftime '%a, %d %b %Y %T GMT'
 	  if File.directory? path
 	    [200, {'Content-Type' => 'text/plain'}, [`ls -al #{path}`] ]
 	  elsif File.extname(path) == '.html'
 	    [200, {'Content-Type' => 'text/html'}, File.open(path, File::RDONLY)]
+	  elsif File.extname(path) == '.css'
+	    [200, {'Content-Type' => 'text/css'}, File.open(path, File::RDONLY)]
 	  else
-	    [200, {'Content-Type' => 'text/plain'}, [File.read(path)]]
+	    [200, {'Content-Type' => 'text/plain', 'Last-Modified' => last_mod}, [File.read(path)]]
 	  end
 	else
 	  [404, {'Content-Type' => 'text/plain'}, ["#{path} does not exist"] ]
